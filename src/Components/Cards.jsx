53bffd54e3dd5e23cc1c6d75781dfef85ab517c4
Bryce DeSantis <potreader@hotmail.com>
2023-12-06 14:23:31 -0500

    Display cards to homepage from API

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,21 @@
+import "../styles/Cards.css"
+
+function Card({ title, image, price }) {
+	return (
+		<div className="card">
+			<img className="card-image" src={image} alt={title} />
+			<div className="card-body">
+				<div className="card-head">
+					<h2 className="card-title">{title}</h2>
+					<h3 className="card-price">
+						<span className="material-symbols-outlined">attach_money</span>
+						{price}
+					</h3>
+				</div>
+				<button className="card-button">Add to cart</button>
+			</div>
+		</div>
+	)
+}
+
+export default Card
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react"
+import Card from "./Cards"
+import "../styles/Homepage.css"
+
 function Homepage() {
+	const [products, setProducts] = useState([])
+
+	async function getProducts() {
+		const fetchedProducts = []
+
+		const response = await fetch("https://fakestoreapi.com/products")
+		// const { id, title, description, price, image } = await response.json()
+		const data = await response.json()
+		for (let i = 0; i < data.length; i++) {
+			const { id, title, price, image } = data[i]
+			fetchedProducts.push({
+				id: id,
+				title: title,
+				price: price,
+				image: image,
+			})
+		}
+		setProducts([...fetchedProducts])
+	}
+
+	useEffect(() => getProducts)
+
 	return (
-		<>
-			<h1>This is the homepage</h1>
-		</>
+		<div className="products-main-section">
+			{products.map((product) => {
+				return (
+					<Card
+						key={product.id}
+						title={product.title}
+						image={product.image}
+						price={product.price}
+					/>
+				)
+			})}
+		</div>
 	)
 }
 
